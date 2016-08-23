dc3876f0fb0022fa120c199564208a86bd7d861d
Phong <p7157568@gmail.com>
2016-08-23 20:55:58 +0700

    feat - add value v19 and v21

--- app/src/main/java/dhbk/android/materialtemplate/activities/MainActivity.java
+++ app/src/main/java/dhbk/android/materialtemplate/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package dhbk.android.materialtemplate;
+package dhbk.android.materialtemplate.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dhbk.android.materialtemplate.R;
 
 public class MainActivity extends AppCompatActivity {
 
