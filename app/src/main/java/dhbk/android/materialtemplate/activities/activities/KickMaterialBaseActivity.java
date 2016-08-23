4467f5f54ac8b2ef1afb360cf276f2bc4ae2b3a4
Phong <p7157568@gmail.com>
2016-08-23 22:46:43 +0700

    feat - add activity

---
+++ app/src/main/java/dhbk/android/materialtemplate/activities/activities/KickMaterialBaseActivity.java
@@ -0,0 +1,7 @@
+package dhbk.android.materialtemplate.activities.activities;
+
+/**
+ * Created by huynhducthanhphong on 8/23/16.
+ */
+public abstract class KickMaterialBaseActivity extends BaseAppCompatActivity implements KickMaterialFragment.HostActivity, NavigationDrawerFragment.NavigationDrawerCallbacks {
+}
--- app/src/main/java/dhbk/android/materialtemplate/activities/activities/MainActivity.java
+++ app/src/main/java/dhbk/android/materialtemplate/activities/activities/MainActivity.java
@@ -1,11 +1,11 @@
-package dhbk.android.materialtemplate.activities;
+package dhbk.android.materialtemplate.activities.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import dhbk.android.materialtemplate.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends KickMaterialBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
