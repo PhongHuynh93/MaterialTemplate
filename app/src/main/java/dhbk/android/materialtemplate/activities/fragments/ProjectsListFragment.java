5355ef738479673b9a506e0cc888d819e2051bc2
Phong <p7157568@gmail.com>
2016-08-24 00:04:38 +0700

    feat - add parcel lib

--- app/src/main/java/dhbk/android/materialtemplate/activities/fragments/ProjectsListFragment.java
+++ app/src/main/java/dhbk/android/materialtemplate/activities/fragments/ProjectsListFragment.java
@@ -1,12 +1,56 @@
 package dhbk.android.materialtemplate.activities.fragments;
 
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+
+import com.byoutline.cachedfield.FieldState;
 import com.byoutline.cachedfield.FieldStateListener;
 
+import javax.annotation.Nullable;
+
 import dhbk.android.materialtemplate.activities.adapters.ProjectClickListener;
+import dhbk.android.materialtemplate.activities.adapters.SharedViews;
+import dhbk.android.materialtemplate.activities.views.EndlessRecyclerView;
 
 /**
  * Created by huynhducthanhphong on 8/23/16.
  */
 public class ProjectsListFragment  extends KickMaterialFragment implements ProjectClickListener, FieldStateListener, EndlessRecyclerView.EndlessScrollListener {
+    /**
+     * Endless scroll variables *
+     */
+    private GridLayoutManager layoutManager;
 
+    public static ProjectsListFragment newInstance(@Nullable Category category) {
+        ProjectsListFragment instance = new ProjectsListFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_CATEGORY, Parcels.wrap(category));
+        instance.setArguments(args);
+        return instance;
+    }
+
+    @Override
+    public int getLastVisibleItemPosition() {
+        return layoutManager.findLastVisibleItemPosition();
+    }
+
+    @Override
+    public void loadMoreData() {
+
+    }
+
+    @Override
+    public boolean hasMoreDataAndNotLoading() {
+        return false;
+    }
+
+    @Override
+    public void fieldStateChanged(FieldState newState) {
+
+    }
+
+    @Override
+    public void projectClicked(int position, SharedViews views) {
+
+    }
 }
