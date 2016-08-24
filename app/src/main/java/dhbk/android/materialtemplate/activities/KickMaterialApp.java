738e4d1555e929aebf31e31841b5a5f0927f996c
Phong <p7157568@gmail.com>
2016-08-24 10:21:27 +0700

    feat - add action style

---
+++ app/src/main/java/dhbk/android/materialtemplate/activities/KickMaterialApp.java
@@ -0,0 +1,29 @@
+package dhbk.android.materialtemplate.activities;
+
+import com.byoutline.secretsauce.BaseApp;
+import com.byoutline.secretsauce.utils.ViewUtils;
+
+import dhbk.android.materialtemplate.BuildConfig;
+
+/**
+ * Created by huynhducthanhphong on 8/24/16.
+ */
+public class KickMaterialApp extends BaseApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+        AndroidStubServer.start(this, NetworkType.UMTS);
+        RetrofitHelper.MSG_DISPLAYER = msg -> ViewUtils.showToast(msg, true);
+        resetComponents();
+    }
+
+    @Override
+    protected boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
+}
